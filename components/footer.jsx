5bd2f60aad660feb73788fa8f65003abfdfe364d
SherlockYigit <sherlockyigit.dev@gmail.com>
2021-06-09 19:27:13 +0300

    Footer update.

--- components/footer.jsx
+++ components/footer.jsx
@@ -3,13 +3,22 @@ import { IoHeartSharp } from 'react-icons/io5';
 function Footer({ socialMedias }) {
   return (
     <footer className="flex justify-around mt-4">
-	  <div className="flex flex-row">
+      <div className="flex flex-row">
         <p className="font-medium text-base"> 
-	      This website was developed by <a className="text-[#38d7b1]" href="https://github.com/SherlockYigit">Yiğit</a> with
-	    </p>
-	    <IoHeartSharp className="mt-1 font-medium text-base text-red-600"/>
-	  </div>
-	</footer>
+	  This website was developed by <a className="text-[#38d7b1]" href="https://github.com/SherlockYigit">Yiğit</a> with
+	</p>
+	<IoHeartSharp className="mt-1 font-medium text-base text-red-600"/>
+      </div>
+      <div className="flex space-x-2">
+      {socialMedias.map((scM, i) => {
+        return (
+	  <a key={`scm-${i}`} href={scM.to} className="px-2 py-2 rounded-full bg-[#d9d8d8] dark:bg-[#260d50]">
+	    <scM.icon className="w-6 h-6"/>
+	  </a>
+        );
+      })}
+      </div>
+    </footer>
   );
 }
 export default Footer
--- config.js
+++ config.js
@@ -1,51 +1,67 @@
+import { FaGithub, FaSpotify, FaDiscord } from "react-icons/fa";
+
 export default ({
   sitename: "SherlockYigit",
   hostname: "website-yigitsh.vercel.app",
   personalInfo: {
-	discordID: "304347029046558721",
+    discordID: "304347029046558721",
     name: "Yiğit",
-	position: "Full Stack Developer",
-	birthday: {
+    position: "Full Stack Developer",
+    birthday: {
       day: Number(process.env.BIRTHDAY), 
-	  month: Number(process.env.BIRTHMONTH), 
-	  year: Number(process.env.BIRTHYEAR), 
-	  gmt: "GMT+3:00", 
-	  time: process.env.BIRTHTIME
-	},
-	description: "Hello 👋, I'm a student. I'm {age} years old now and I have some small projects. I spend more time from classes on my projects and cycling.",
-	favouriteArtists: [
+      month: Number(process.env.BIRTHMONTH), 
+      year: Number(process.env.BIRTHYEAR), 
+      gmt: "GMT+3:00", 
+      time: process.env.BIRTHTIME
+    },
+    description: "Hello 👋, I'm a student. I'm {age} years old now and I have some small projects. I spend more time from classes on my projects and cycling.",
+    favouriteArtists: [
       { 
-	    name: "Duman", 
-		image: "/artists/duman.webp" 
-	  }, 
+	name: "Duman", 
+	image: "/artists/duman.webp" 
+      }, 
       { 
-	    name: "Model", 
-		image: "/artists/model.webp" 
-	  },
+	name: "Model", 
+	image: "/artists/model.webp" 
+      },
       { 
-	    name: "Adamlar", 
-		image: "/artists/adamlar.webp" 
-	  },
+	name: "Adamlar", 
+	image: "/artists/adamlar.webp" 
+      },
       { 
-	    name: "Gin Wigmore", 
-		image: "/artists/ginwigmore.webp" 
-	  },
+	name: "Gin Wigmore", 
+	image: "/artists/ginwigmore.webp" 
+      },
       { 
-	    name: "Dorothy", 
-		image: "/artists/dorothy.webp" 
-	  },
+	name: "Dorothy", 
+	image: "/artists/dorothy.webp" 
+      },
       { 
-	    name: "Ece Seçkin", 
-		image: "/artists/ece_seckin.webp" 
-	  }
-	]
+	name: "Ece Seçkin", 
+	image: "/artists/ece_seckin.webp" 
+      }
+    ],
+    socialMedias: [
+      {
+        to: "https://github.com/SherlockYigit",
+	icon: FaGithub
+      },
+      {
+        to: "https://open.spotify.com/user/m0jtc4mufe1ypfs6if1ab7dzl",
+	icon: FaSpotify
+      },
+      {
+        to: "https://discord.com/channels/@me/304347029046558721",
+	icon: FaDiscord 
+      }
+    ]
   },
   routes: [
     { 
-	  isMaintenance: true, 
-	  name: "Blog", 
-	  to: "/blog", 
-	  description: "" 
-	}
+      isMaintenance: true, 
+      name: "Blog", 
+      to: "/blog", 
+      description: "Personal blog." 
+    }
   ]
 });
