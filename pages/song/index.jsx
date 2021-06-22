f1843529a764fe2c5fcfa6438c85f9c783d8fd6e
Burak DOĞAN <ykafasiyt@gmail.com>
2021-06-22 12:49:11 +0300

    Update index.jsx

--- pages/song/index.jsx
+++ pages/song/index.jsx
@@ -3,14 +3,6 @@ import Footer from "../../components/footer.jsx";
 import Spotify from "../../components/Song/spotify.jsx";
 import Config from "../../config.js";
 
-function Index() {
-  return (
-    <div>
-      <Navbar title="Song" description="Song"/>
-      <Footer socialMedias={Config.personalInfo.socialMedias} />
-    </div>
-  );
-}
 class Index extends Component {
   constructor(props) {
     super(props);
