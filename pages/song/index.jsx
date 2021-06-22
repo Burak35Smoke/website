c8e4f9d29019597bfe1c63f9e4171fa4202ae052
Burak DOĞAN <ykafasiyt@gmail.com>
2021-06-22 12:43:32 +0300

    Update index.jsx

--- pages/song/index.jsx
+++ pages/song/index.jsx
@@ -1,6 +1,6 @@
 import Navbar from "../../components/Song/navbar.jsx";
 import Footer from "../../components/footer.jsx";
-import Spotify from "../components/Song/spotify.jsx";
+import Spotify from "../../components/Song/spotify.jsx";
 import Config from "../../config.js";
 
 function Index() {
